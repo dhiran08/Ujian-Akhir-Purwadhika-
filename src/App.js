30a3b5424f384b12888cf6b78cb36fae82d2defc
Dhiran08 <dhiranandi08@gmail.com>
2018-12-10 13:32:27 +0700

    coba masukkan folder baru + naruh gambar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Jam from './component/Jam';
+import Gambar1 from './component/Gambar/Coba5.png';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={Gambar1} className="Gambar1" alt="Gambar1" />
           <Jam />
         </header>
       </div>
