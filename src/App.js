5573289a1361a4c86890ec971e83e510a90b5283
Dhiran08 <dhiranandi08@gmail.com>
2018-12-11 10:24:51 +0700

    buat yang sebenarnya, hehe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Jam from './component/Jam';
-import Gambar1 from './component/Gambar/Coba5.png';
+import Gambar1 from './component/Gambar/62843.jpg';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,35 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={Gambar1} className="Gambar1" alt="Gambar1" />
+          <a href='https://www.freepik.com/free-vector/silhouette-of-the-taj-mahal_3462548.htm' alt="coba">Designed by Rawpixel.com </a>
+          <h1>
+            Percobaan Membuat Jadwal Sholat
+          </h1>
           <Jam />
+
+          <table>
+            <tr>
+              <td>Subuh</td>
+              <td>Dzuhur</td>
+              <td>Ashar</td>
+              <td>Maghrib</td>
+              <td>Isya</td>
+            </tr>
+            <tr>
+              <td>04.08</td>
+              <td>11.47</td>
+              <td>15.14</td>
+              <td>18.02</td>
+              <td>19.18</td>
+            </tr>
+            <tr>
+              <td>Catatan:</td>
+              <td>disamping ini maunya ditulis hitungan mundur berapa jam, menit, detik lagi sebelum waktu sholatnya, jadi ada live time hitungan mundur. kalau sudah 10 menit terakhir sebelum masuk waktu sholat, otomatis berubah merah tulisannya</td>
+            </tr>
+          </table>
+          <h1 color="red" /*ini kok dikasih merah ndak merah yaa? salah sintaks mungkin...*/>masih mencari tahu caranya,....</h1>
+          <h1> Catatan tambahan: </h1>
+          <h3> Jadwal sholat kan setiap harinya berbeda-beda kadang beda beberapa menit saja sih. berdasarkan apanya juga masih belum tau karena ada hitungan-hitungannya. bagaimana cara setiap harinya otomatis jadwal itu berganti?</h3>
         </header>
       </div>
     );
